d62f20ae41155c3a25be8a6e7c01d57660167efb
Alex Lukens <alex.lukens@canonical.com>
2025-06-06 15:52:18 -0400

    fix strenum errors encountered during build

--- tests/validate/test_status_severity.py
+++ tests/validate/test_status_severity.py
@@ -3,8 +3,8 @@ from juju.status import StatusStr, severity_map
 
 def test_status_str_values():
     for name, value in StatusStr._member_map_.items():
-        assert name == value
+        assert name == value.value
 
 
 def test_severity_map():
-    assert set(StatusStr._member_names_) == set(severity_map)
+    assert set(StatusStr._member_names_) == set([item.value for item in severity_map])
